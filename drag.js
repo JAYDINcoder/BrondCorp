08dc3000d0c69533bbc9293ea16332b13e6016b5
jaydin12341123 <95945966+jaydin12341123@users.noreply.github.com>
2024-06-20 18:24:20 -0500

    garlic.exe

--- drag.js
+++ drag.js
@@ -7,11 +7,16 @@ var Butt2 = document.querySelector(".icon2")
 var cool = document.querySelector(".endthing")
 var power = document.querySelector("#power")
 var Butt3 = document.querySelector(".icon3")
+var Butt4 = document.querySelector(".icon4")
 var Vomit = document.querySelector(".containvomit")
 var snd = new Audio("Yell.wav"); 
+var dance = document.querySelector(".Garlic")
+var strobe = document.querySelector(".Party")
+var music = new Audio("Fart.mp3");
 
 Window2.style.display = "none"
 cool.style.display = "none"
+strobe.style.display = "none"
 Window.style.display = "none"
 Window.style.opacity = 0;
 var move = false;
@@ -40,6 +45,22 @@ Butt2.addEventListener('click', function() {
         newAudio.play()
         Vomit.style.animation="drop 6s Linear"
     }, 100)
+  });
+  Butt4.addEventListener('click', function() {
+    dance.style.animation="none"
+    setTimeout(function(){
+        strobe.style.opacity = "0.4"
+        strobe.style.display = "block"
+        dance.style.animation="grove 4s Linear"
+        music.play();
+    }, 100)
+    setTimeout(function(){
+        fade()
+    }, 4000)
+    setTimeout(function(){
+        strobe.style.display = "none"
+        music.stop();
+    }, 5700)
   });
 
 End.addEventListener('click', function() {
@@ -111,7 +132,22 @@ function close(element) {
         op += op * 0.1;
     }, 10);
 }
-
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            strobe.style.display = 'none';
+        }
+        strobe.style.opacity = op;
+        strobe.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+Audio.prototype.stop = function() {
+    music.pause();
+    music.currentTime = 0;
+};
 Window.addEventListener("mousedown",()=>{
     move = true;
 })
